f2df9ad448661170f519cf8631829af609734615
AskePetersen <aske98@live.com>
2021-10-17 23:04:30 +0200

    cluster friendly notebook.py

--- oyvin_code/notebook.py
+++ oyvin_code/notebook.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
 from Image_Functions import slicing, crop_images_to_brain, crop_to_size
@@ -12,6 +6,10 @@ from Model import CNN
 from Dice_Loss import DiceLoss
 import torch
 import os
+from datetime import datetime
+import nibabel as nib
+import torch.nn as nn
+from matplotlib import pyplot as plt
 
 #hyper parameters
 batch_size = 2
@@ -21,14 +19,17 @@ num_epochs = 4
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
-from matplotlib import pyplot as plt
-
 "Need to specify the local path on computer"
-dir_path = os.path.join(os.getcwd(), 'Cropped_Task3')
-print(dir_path)
+test_path = os.path.join(os.getcwd(), 'Biomedical_Image_Segmentation')
+print(test_path)
+test_imgur = nib.load(os.path.join(test_path, "Cropped_Task3/crop_sub-233/crop_sub-233_space-T1_desc-masked_T1.nii.gz"))
 
+def save_image(data, affine):
+    cropped_img = nib.Nifti1Image(data, affine)
+    nib.save(cropped_img, "test.nii.gz")
 
 'Splitting the data into 30% test and 70% training.'
+dir_path = os.path.join(os.getcwd(), "Biomedical_Image_Segmentation\Cropped_Task3")
 X_train, X_test = train_test_split(Set(dir_path, sub_dir = 'crop_sub-23'), test_size=0.3, random_state=25)
 size = (256,288,176)
 X_train, X_test = crop_to_size(X_train, size), crop_to_size(X_test,size)
@@ -37,8 +38,10 @@ X_train, X_test = crop_to_size(X_train, size), crop_to_size(X_test,size)
 train_loader, test_loader = DataLoader(X_train, batch_size=1), DataLoader(X_test, batch_size=1)
 
 'Run the CNN'
-'Run the CNN'
-model = CNN(3,base_features=16).to(device)
+model = CNN(3,base_features=4)
+model = nn.DataParallel(model)
+model.to(device)
+
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 n_total_steps = len(train_loader)
 loss_func_2 = DiceLoss()
@@ -48,19 +51,12 @@ for epoch in range(num_epochs):
     for i, image_set in enumerate(train_loader):
         image = image_set['data'].to(device)
         labels = image_set['seg'].to(device)
-        optimizer.zero_grad()
+        optimizer.zero_grad() #I'd put it further down, it might not make a difference
         outputs = model(image)
-
-        print("outputs shape = ", outputs.shape)
-        print("labels shape = ", labels.shape)
         loss = loss_func_2(outputs, labels)
-        print(loss.item())
         losses.append(loss.item())
-        print(2)
         loss.backward()
-        print(3)
         optimizer.step()
-        print(4)
         if (i+1) % 1 == 0:
             print(f'epoch {epoch+1} / {num_epochs}, step {i+1}/{n_total_steps}, loss = {loss.item():.4f}')
 
@@ -71,10 +67,9 @@ print(losses)
 test_pred = iter(test_loader)
 test_img = test_pred.next()
 
-prediction = model(test_img['data'])
+prediction = model(test_img['data'].to(device))
 
-slicing(prediction, 100, 150, 80)
-
-
-
-
+save_image(torch.squeeze(prediction.detach()).cpu().numpy(), test_imgur.affine)
+time = datetime.now().replace(microsecond=0).strftime("kl%H%M%S-%d.%m.%Y")
+print("The saved name of the file was = ", str(time) , ".csv")
+torch.save(prediction, str(time) + ".csv")
