65be7d8fc016bcb25a5c48c0e3ecaa3a1beb4946
Øyvin Moxness <oyvin.moxvoll@gmail.com>
2021-11-04 15:00:09 +0100

    aD E

--- crop_and_save.py
+++ crop_and_save.py
@@ -3,11 +3,15 @@ import nibabel as nib
 import numpy as np
 import os
 import shutil
-import torchio as tio
-
-
-dir_path = 'Task3/'
-new_dir = 'Cropped_Task3/'
+import numpy as np
+from numpy import save
+from numpy import savez_compressed
+import os
+import nibabel as nib
+dir_path = 'Cropped_Task3/'
+new_dir = 'Numpy_Comp_Task3/'
+seg_dir_path = 'Cropped_Task3/Segmentations/'
+seg_new_dir = 'Numpy_Task3/Segmentations/'
 
 
 
@@ -33,55 +37,29 @@ class Crop_And_Save():
                     and 'Lacunes' in name]) 
             return data, seg
 
-    def extract_brain_region(image, brain_mask, background=0.):
-        ''' find the boundary of the brain region, return the resized brain image and the index of the boundaries'''    
-        brain = np.where(brain_mask != background)
-        min_z = int(np.min(brain[0]))
-        max_z = int(np.max(brain[0]))+1
-        min_y = int(np.min(brain[1]))
-        max_y = int(np.max(brain[1]))+1
-        min_x = int(np.min(brain[2]))
-        max_x = int(np.max(brain[2]))+1
-        # resize image
-        resizer = (slice(min_z, max_z), slice(min_y, max_y), slice(min_x, max_x))
-        return resizer
-
     def make_new_folder(self, folder):
         try:
-            os.mkdir(os.path.join(self.new_dir, 'crop_' + folder))
+            os.mkdir(os.path.join(self.new_dir, 'numpy_' + folder))
         except:
             print('Folder allready exists')
-
-    def save_image(self, data, affine, header, path):
-        cropped_img = nib.Nifti1Image(data, affine, header)
-        nib.save(cropped_img, path)
 
     def crop_and_save(self):
         for indx in range(len(self.sub_folders)):
             self.make_new_folder(self.sub_folders[indx])
             path = os.path.join(self.data_path, self.sub_folders[indx])
-            new_path = os.path.join(self.new_dir, 'crop_'+self.sub_folders[indx])
+            new_path = os.path.join(self.new_dir, 'numpy_'+self.sub_folders[indx])
             print(self.sub_folders[indx])
             data,seg = self.get_folder_content(indx)
-            img = nib.load(os.path.join(path, data[0]))
-            img_data = img.get_fdata()
-            new_size = self.extract_brain_region(img_data)
-            img = None
-            img_data = None
             for elm in data:
                 image = nib.load(os.path.join(path, elm))
                 img_data = image.get_fdata()
-                img_data = img_data[new_size]
-                self.save_image(img_data, image.affine, image.header, os.path.join(new_path, 'crop_' + elm))
+                elm = elm.replace('.nii.gz', '.npz')
+                savez_compressed(os.path.join(new_path, elm), img_data)
             for elm in seg:
                 image = nib.load(os.path.join(path, elm))
                 img_data = image.get_fdata()
-                max_pre = img_data.max()
-                print('Max Value: Segmentation: ', max_pre)
-                img_data = img_data[new_size]
-                max_post = img_data.max()
-                assert max_pre == max_post, print('Data Lost when cropping Segmentation image: %s' % elm)
-                self.save_image(img_data, image.affine, image.header, os.path.join(new_path, 'crop_' + elm))  
+                elm = elm.replace('.nii.gz', '.npz')
+                savez_compressed(os.path.join(new_path, elm), img_data)
 
     def undo(self):
         if os.path.exists(self.new_dir):
@@ -105,7 +83,10 @@ class Crop_And_Save():
 
 
 
-test = Crop_And_Save(dir_path, new_dir, sub_dir='sub-2')
+test = Crop_And_Save(dir_path, new_dir, sub_dir='sub-')
+test.crop_and_save()
+
+test = Crop_And_Save(seg_dir_path, seg_new_dir, sub_dir='sub-')
 test.crop_and_save()
 
 
