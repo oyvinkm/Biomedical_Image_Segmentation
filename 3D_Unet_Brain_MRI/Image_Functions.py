8c9129d205fc3e5ca53e1800f63648ce33f38333
AskePetersen <aske98@live.com>
2021-11-10 12:02:16 +0100

    get_test_loss()

---
+++ 3D_Unet_Brain_MRI/Image_Functions.py
@@ -0,0 +1,61 @@
+from matplotlib import pyplot as plt
+import numpy as np
+import torchio as tio
+import os
+
+def show_slices(slices, color = 'gray'):
+   """ Function to display row of image slices """
+   fig, axes = plt.subplots(1, len(slices))
+   for i, slice in enumerate(slices):
+       axes[i].imshow(slice.T, cmap=color, origin="lower")
+
+def slicing(img, x,y,z, cmap = 'gray'):
+    slice_0 = img[x, :, :]
+    slice_1 = img[:, y, :]
+    slice_2 = img[:, :, z]
+    show_slices([slice_0, slice_1, slice_2], color = cmap)
+    plt.suptitle("Center slices for EPI image") 
+    plt.show()
+
+def save_slice(img, folder_name, size: tuple =(64,64,64), cmap='gray'):
+    slice_0 = img[size[0], :, :]
+    slice_1 = img[:, size[1], :]
+    slice_2 = img[:, :, size[2]]
+    show_slices([slice_0, slice_1, slice_2], color = cmap)
+    plt.suptitle("Center slices for EPI image" ) 
+    plt.savefig(folder_name)
+    plt.close()
+
+def extract_brain_region(image, brain_mask, seg, background=0.):
+	''' find the boundary of the brain region, return the resized brain image and the index of the boundaries'''    
+	brain = np.where(brain_mask != background)
+	min_z = int(np.min(brain[1]))
+	max_z = int(np.max(brain[1]))+1
+	min_y = int(np.min(brain[2]))
+	max_y = int(np.max(brain[2]))+1
+	min_x = int(np.min(brain[3]))
+	max_x = int(np.max(brain[3]))+1
+	# resize image
+	resizer = (slice(0,3),slice(min_z, max_z), slice(min_y, max_y), slice(min_x, max_x))
+	return image[resizer], seg[resizer], [[min_z, max_z], [min_y, max_y], [min_x, max_x]]
+
+
+def crop_images_to_brain(data_set):
+    min_z, min_y, min_x = np.inf, np.inf, np.inf
+    for i in range(len(data_set)):
+        elm = data_set[i]
+        cropped_img, cropped_seg, new_size = extract_brain_region(elm['data'], elm['data'], elm['seg'])
+        if (new_size[0][1] - new_size[0][0] < min_z): min_z = new_size[0][1] - new_size[0][0]
+        if (new_size[1][1] - new_size[1][0] < min_y): min_y = new_size[1][1] - new_size[1][0]
+        if (new_size[2][1] - new_size[2][0] < min_x): min_x = new_size[2][1] - new_size[2][0]
+        data_set[i]['data'], data_set[i]['seg'] = cropped_img, cropped_seg
+    resizer = (slice(0,3),slice(0, min_z), slice(0, min_y), slice(0, min_x))
+    for i in range(len(data_set)):
+        data_set[i]['data'], data_set[i]['seg'] = data_set[i]['data'][resizer], data_set[i]['seg'][resizer]
+    return data_set
+
+def crop_to_size(set, size):
+    for i in range(len(set)):
+        transform = tio.CropOrPad((size))
+        set[i]['data'], set[i]['seg'] = transform(set[i]['data']), transform(set[i]['seg'])
+    return set
---
+++ 3D_Unet_Brain_MRI/Model.py
@@ -0,0 +1,241 @@
+import torch 
+from torch import nn
+
+
+
+
+"Simple neural network with one convolution and activation"
+class CNN(nn.Module):
+    def __init__(self, in_channels, base_features : int, deep_supervision = False):
+        super(CNN, self).__init__()
+        self.in_channels =  in_channels
+        self.base_features = base_features
+        self.deep_supervision = deep_supervision
+        self.maxpool = nn.MaxPool3d
+        self.upconv = nn.ConvTranspose3d
+        #Layer Encode 1
+        self.conv1_layer1 = self._conv_layer_set(self.in_channels, self.base_features)
+        self.conv1_layer2 = self._conv_layer_set(self.base_features, self.base_features)
+        self.maxPool1 = self.maxpool((2,2,2))
+
+        #Layer Encode 2
+        self.conv2_layer1 = self._conv_layer_set(self.base_features, self.base_features*2)
+        self.conv2_layer2 = self._conv_layer_set(self.base_features*2, self.base_features*4)
+        self.maxPool2 = self.maxpool((2,2,2))
+
+        #Layer Encode 3
+        self.conv3_layer1 = self._conv_layer_set(self.base_features*4, self.base_features*4)
+        self.conv3_layer2 = self._conv_layer_set(self.base_features*4, self.base_features*8)
+        self.maxPool3 = self.maxpool((2,2,2))
+
+        #Bottom Layer
+        self.conv4_layer1 = self._conv_layer_set(self.base_features*8, self.base_features*8)
+        self.conv4_layer2 = self._conv_layer_set(self.base_features*8, self.base_features*16)
+        self.upconv1 = self.upconv(self.base_features*16, self.base_features*16, (2,2,2), 2)
+
+        #Decode Layer 1
+        self.conv5_layer1 = self._conv_layer_set((self.base_features*8 + self.base_features*16), self.base_features*4)
+        self.conv5_layer2 = self._conv_layer_set(self.base_features*4, self.base_features*4)
+        self.supervised_layer5 = nn.Sequential(nn.Conv3d(self.base_features*8, 1, kernel_size=3, stride=1, padding=1), nn.Softmax(dim=0))
+        self.upconv2 = self.upconv(self.base_features*4, self.base_features*4, (2,2,2), 2)
+
+        #Decode Layer 2
+        self.conv6_layer1 = self._conv_layer_set((self.base_features*4 + self.base_features*4), self.base_features*2)
+        self.conv6_layer2 = self._conv_layer_set(self.base_features*2, self.base_features*2)
+        self.supervised_layer6 = nn.Sequential(nn.Conv3d(self.base_features*4, 1, kernel_size=3, stride=1, padding=1), nn.Softmax(dim=0))
+        self.upconv3 = self.upconv(self.base_features*2, self.base_features*2, (2,2,2), 2)
+        
+        #Decode Layer 3
+        self.conv7_layer1 = self._conv_layer_set((self.base_features + self.base_features*2), self.base_features)
+        self.conv7_layer2 = self._conv_layer_set(self.base_features, self.base_features)
+        #self.supervised_layer7 = nn.Sequential(nn.Conv3d(self.base_features*4, 1, kernel_size=3), nn.Softmax())
+        #Output
+        self.final_conv = nn.Sequential(nn.Conv3d(self.base_features, 1, kernel_size=1, stride=1),
+                          nn.Sigmoid())
+
+
+        """ self.encoder_blocks = nn.ModuleList(self.build_block('encode'))
+        self.decoder_blocks = nn.ModuleList(self.build_block('decode'))
+        self.depth = 4  """
+        
+
+    def _conv_layer_set(self, feat_in, feat_out):
+            conv_layer = nn.Sequential(
+            nn.Conv3d(feat_in, feat_out, kernel_size = 3, stride=1, padding=1), nn.BatchNorm3d(feat_out),
+            nn.LeakyReLU()
+            )
+            return conv_layer
+    """def encoder_block(self, feat_in, feat_out, feat_mid = None, maxpool = True):
+        if feat_mid is None:
+            feat_mid = feat_in
+        conv1 = self._conv_layer_set(feat_in, feat_mid)
+        if maxpool:
+            block = nn.Sequential(conv1, conv2)
+        else:
+            block = nn.Sequential(conv1,conv2, nn.ConvTranspose3d(feat_out, feat_out, 2, 2))
+        return block
+
+
+    def decoder_block(self, feat_in, feat_out, feat_uncat = None):
+        if feat_uncat is None:
+            conv1 = self._conv_layer_set(feat_in, feat_out)
+        else:
+        conv2 = self._conv_layer_set(feat_mid, feat_out)
+            conv1 = self._conv_layer_set(feat_in, feat_uncat)
+        conv2 = self._conv_layer_set(feat_out, feat_out)
+        block = nn.Sequential(conv1, conv2, nn.ConvTranspose3d(feat_out, feat_out, 2, 2))
+        return block
+
+    def build_block(self, path_type):
+        block_list = []
+        if (path_type == 'encode'):
+            print('encoding')
+            block_list.append(self.encoder_block(3, 64, 32))
+            block_list.append(nn.MaxPool3d(2,2))
+            block_list.append(self.encoder_block(64, 128))
+            block_list.append(nn.MaxPool3d(2,2))
+            block_list.append(self.encoder_block(128,256))
+            block_list.append(nn.MaxPool3d(2,2))
+            block_list.append(self.encoder_block(256, 512))
+            block_list.append(nn.ConvTranspose3d(512, 512, 2, 2))
+        elif (path_type == 'decode'):
+            print('decoding')
+            block_list.append(self.decoder_block(768, 256))
+            block_list.append(self.decoder_block(384,128))
+            block_list.append(self.decoder_block(192, 64))
+        else: 
+            print('Given wrong type')
+        return block_list
+
+    def _get_length_(self):
+        print('Encoder Length: ', len(self.encoder_blocks))
+        print('Decoder Length: ', len(self.decoder_blocks)) 
+            
+    def forward(self, x):
+        concat = []
+        out = x.float()
+        for d in range(len(self.encoder_blocks) - 2):
+            out = self.encoder_blocks[d](out)
+            if not isinstance(self.encoder_blocks[d], nn.MaxPool3d):
+                concat.append(out)
+        for i in range(len(concat)):
+            print('Concat: ', concat[i].shape)
+        out = self.encoder_blocks[-2](out)
+        out = self.encoder_blocks[-1](out)
+        for u in range(len(self.decoder_blocks)):
+            print(concat[-1].shape)
+            print(out.shape)
+            out = torch.cat((concat.pop(), out), dim = 1)
+            print('Concatted')
+            out = self.decoder_blocks[u](out)
+            print('Decode: ', out.shape)
+        out = self.final_conv(out) """
+
+    def forward(self, x):
+        if not self.deep_supervision:
+            out = x.float()
+            skips = []
+            #Encode Layer 1
+            out = self.conv1_layer1(out)
+            out = self.conv1_layer2(out)
+        
+            skips.append(out)
+            out = self.maxPool1(out)
+
+            #Encode Layer 2
+            out = self.conv2_layer1(out)
+            out = self.conv2_layer2(out)
+        
+            skips.append(out)
+            out = self.maxPool2(out)
+            
+
+            #Encode Layer 3
+            out = self.conv3_layer1(out)
+            out = self.conv3_layer2(out)
+        
+            skips.append(out)
+            out = self.maxPool3(out)
+
+            #Bottom Layer
+            out = self.conv4_layer1(out)
+            out = self.conv4_layer2(out)
+        
+            out = self.upconv1(out)
+
+            #Decode Layer 1
+            out = torch.cat((skips.pop(), out), dim=1)
+            out = self.conv5_layer1(out)
+            out = self.conv5_layer2(out)
+            
+            out = self.upconv2(out)
+            
+            #Decode Layer 2
+            out = torch.cat((skips.pop(), out), dim=1)
+            out = self.conv6_layer1(out)
+            out = self.conv6_layer2(out)
+            
+            out = self.upconv3(out)
+
+            #Decode Layer 3
+            out = torch.cat((skips.pop(), out), dim=1)
+            out = self.conv7_layer1(out)
+            out = self.conv7_layer2(out)
+
+            #Final
+            out = self.final_conv(out)
+            return out
+        else:
+            out = x.float()
+            skips = []
+            layers = []
+            #Encode Layer 1
+            out = self.conv1_layer1(out)
+            out = self.conv1_layer2(out)
+        
+            skips.append(out)
+            out = self.maxPool1(out)
+
+            #Encode Layer 2
+            out = self.conv2_layer1(out)
+            out = self.conv2_layer2(out)
+        
+            skips.append(out)
+            out = self.maxPool2(out)
+            
+
+            #Encode Layer 3
+            out = self.conv3_layer1(out)
+            out = self.conv3_layer2(out)
+        
+            skips.append(out)
+            out = self.maxPool3(out)
+
+            #Bottom Layer
+            out = self.conv4_layer1(out)
+            out = self.conv4_layer2(out)
+        
+            out = self.upconv1(out)
+
+            #Decode Layer 1
+            out = torch.cat((skips.pop(), out), dim=1)
+            out = self.conv5_layer1(out)
+            out = self.conv5_layer2(out)
+            layers.append(self.supervised_layer5(out))
+            out = self.upconv2(out)
+            
+            #Decode Layer 2
+            out = torch.cat((skips.pop(), out), dim=1)
+            out = self.conv6_layer1(out)
+            out = self.conv6_layer2(out)
+            layers.append(self.supervised_layer6(out))
+            out = self.upconv3(out)
+
+            #Decode Layer 3
+            out = torch.cat((skips.pop(), out), dim=1)
+            out = self.conv7_layer1(out)
+            out = self.conv7_layer2(out)
+
+            #Final
+            layers.append(self.final_conv(out))
+            return layers
