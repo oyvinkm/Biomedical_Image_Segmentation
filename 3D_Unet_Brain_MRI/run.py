5f49782bae10440a355c5e6e43708d8b197a370d
AskePetersen <hlp179@alumni.ku.dk>
2021-11-17 09:57:02 +0100

    Testing functional

--- 3D_Unet_Brain_MRI/run.py
+++ 3D_Unet_Brain_MRI/run.py
@@ -17,9 +17,9 @@ import nibabel as nib
 network = Dynamic_3DUnet #No need to change
 loss_func = BinaryFocalLoss()
 optimizer = torch.optim.Adam
-batch_size = 1
-num_batches_per_epoch = 1 #Number of batches before new epoch
-epochs = 1
+batch_size = 2
+num_batches_per_epoch = 10 #Number of batches before new epoch
+epochs = 75
 patch_size = (128, 128, 128)# Make sure that each value is divisible by 2**(num_pooling)
 in_channels = 3 #No need to change really
 base_features = 4 #Number of base features in 3D
@@ -28,10 +28,10 @@ dialation_prob = 0.6
 dialation_epochs = 50
 dialation = True
 #data_folder = 'Cropped_Task3'
-data_folder = '3segmentations' 
+data_folder = 'Segmentations' 
 out_folder = '3D_Unet_Train'
 sub_dir = 'crop_sub-2'
-alternate_folder = 'Segmentations'
+alternate_folder = '.'
 cluster_path = os.path.join(os.getcwd(), 'Biomedical_Image_Segmentation/Cropped_Task3/crop_sub-233/crop_sub-233_space-T1_desc-masked_T1.nii.gz')
 pc_path = os.path.join(os.getcwd(), 'Segmentations/crop_sub-233/crop_sub-233_space-T1_desc-masked_T1.nii.gz')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -51,8 +51,8 @@ dir_path = os.path.join(os.getcwd(), data_folder) if (os.name == 'nt') else os.p
 data_folders = sorted([folder for folder  in os.listdir(dir_path) if 
                         os.path.isdir(os.path.join(dir_path, folder)) 
                         and sub_dir in folder])
-train, test = train_test_split(data_folders, test_size = 0.3)
-train, val = train_test_split(train, train_size=0.5)
+train, test = train_test_split(data_folders, test_size = 0.15)
+train, val = train_test_split(train, train_size=0.8)
 X_train = Set(dir_path, train)
 X_test = Set(dir_path, test)
 X_val = Set(dir_path, val)
